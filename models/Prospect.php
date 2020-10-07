e33b3be1e174e07f3c1b43256c8bbf58e506bbe8
Welinton Haas <welinton.haas@gmail.com>
2020-10-07 20:05:45 -0300

    Models

--- models/Prospect.php
+++ models/Prospect.php
@@ -6,3 +6,29 @@
 * Data: 30/09/2020
 *
 **************************************************/
+namespace models;
+
+class Prospect 
+{
+    public $codigo;
+    public $nome;
+    public $email;
+    public $celular;
+    public $facebook;
+    public $whatsapp;
+    public $instagram;
+    public $endereco;
+    public $dataNascimento;
+
+    public function addProspect($nome,$email,$celular,$facebook,$whatsapp,$instagram,$endereco,$dataNascimento)
+    {
+        $this->nome             = $nome;
+        $this->email            = $email;
+        $this->celular          = $celular;
+        $this->facebook         = $facebook;
+        $this->whatsapp         = $whatsapp;
+        $this->instagram        = $instagram;
+        $this->endereco         = $endereco;
+        $this->dataNascimento   = $dataNascimento;
+    }
+}
--- models/Usuario.php
+++ models/Usuario.php
@@ -6,3 +6,24 @@
 * Data: 30/09/2020
 *
 **************************************************/
+namespace models;
+
+class Usuario 
+{
+    public $codigo;
+    public $login;
+    public $senha;
+    public $nome;
+    public $email;
+    public $celular;
+
+    public function addUsuario($login,$senha,$nome,$email,$celular)
+    {
+        $this->login    = $login;
+        $this->senha    = $senha;
+        $this->nome     = $nome;
+        $this->email    = $email;
+        $this->celular  = $celular;
+    }
+
+}
