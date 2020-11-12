15760c2d4fb781c274a36edd411e37d6035b6bee
Welinton Haas <welinton.haas@gmail.com>
2020-11-11 21:36:18 -0300

    Testes Prospect

--- tests/ProspectTest.php
+++ tests/ProspectTest.php
@@ -12,18 +12,62 @@ use dao\DAOProspect;
 class ProspectTest extends TestCase{
 
    /** @test */
-   public function testIncluirProspect(){
+   public function testExcluirProspect(){
       $daoProspect = new DAOProspect();
       try{
          $this->assertEquals(
             TRUE,
-            $daoProspect->incluirProspect("Welinton Haas", "welinton@gmail.com", "(49)99992-1631", "@facebookWelinton", "999921631")
+            $daoProspect->excluirProspect(43)
          );
          unset($daoProspect);
       }catch(\Exception $e){
          $this->fail($e->getMessage());
       }
    }
+
+   /** @test */
+   public function testIncluirProspect(){
+      $daoProspect = new DAOProspect();
+      try{
+         $this->assertEquals(
+            TRUE,
+            $daoProspect->incluirProspect("Teste Haas", "um@gmail.com", "(49)21231231", "@facebookWelinton", "999921631")
+         );
+         unset($daoProspect);
+      }catch(\Exception $e){
+         $this->fail($e->getMessage());
+      }
+   }
+
+    /** @test */
+    public function testAtualizarProspect(){
+      $daoProspect = new DAOProspect();
+      try{
+         $this->assertEquals(
+            TRUE,
+            $daoProspect->atualizarProspect(31, "outro", "outro@gmail.com", "1111111111", "@facebookoutro", "222222222")
+         );
+         unset($daoProspect);
+      }catch(\Exception $e){
+         $this->fail($e->getMessage());
+      }
+   }
+
+   /** @test */
+   public function testBuscarProspect(){
+      $daoProspect = new DAOProspect();
+      try{
+         $this->assertEquals(
+            "Gervasio Nunes",
+            $daoProspect->buscarProspect("gernunes@hotmail.com")
+         );
+         unset($daoProspect);
+      }catch(\Exception $e){
+         $this->fail($e->getMessage());
+      }
+   }
+
+  
 
   
 }
